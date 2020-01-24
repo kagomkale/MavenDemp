bbabd60ca5f7ce9ecfc3a0bc7dab5b5016ffce5c
unknown <kagomkale94@gmail.com>
2020-01-24 18:59:52 +0530

    selenium+git+jenkin with maven project

---
+++ src/test/java/com/qa/tests/DemoTest.java
@@ -0,0 +1,37 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class DemoTest {
+	@Test
+	public void add() {
+		int a=10;
+		int b=5;
+		Assert.assertEquals(15,a+b);
+		System.out.println("Sum");
+	}
+	@Test
+	public void sub() {
+		int a=10;
+		int b=5;
+		Assert.assertEquals(5,a-b);
+		System.out.println("Sub");
+	}
+	@Test
+	public void multi() {
+		int a=10;
+		int b=5;
+		Assert.assertEquals(50,a*b);
+		System.out.println("multi");
+	}
+	@Test
+	public void div() {
+		int a=10;
+		int b=5;
+		Assert.assertEquals(2,a/b);
+		System.out.println("div");
+	}
+}
